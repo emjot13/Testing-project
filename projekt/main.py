adbfb9aa99a87681bdcee045ca6c669c0d2af779
Mateusz Jarzembiński <mateuszjarzembinski@wp.pl>
2021-12-21 13:27:17 +0100

    added generic function that throws error when index value is incorrect within the class, created delete_student function

--- projekt/main.py
+++ projekt/main.py
@@ -7,16 +7,21 @@ class E_gradebook:
         if type(string) is not str:
             raise TypeError(f'Given value: "{string}" is not a string')
 
+    def index_error(self, index):
+        if type(index) is not int:
+            raise TypeError("Index must be an integer")
+
     def add_student(self, student):
         self.string_error(student)
         if len(student.split()) < 2:
             raise ValueError("Student must have a name and a surname")
+        self.index += 1
         dic = {self.index: student, "annotations": {}, "average grades": {}}
         new_entry = [dic, {}]
         self.students.append(new_entry)
-        self.index += 1
 
-
+    def delete_student(self, index):
+        self.index_error(index)
 
 
 
