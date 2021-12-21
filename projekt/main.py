42307999ed667b435617da00efc9515447da8536
Mateusz Jarzembiński <mateuszjarzembinski@wp.pl>
2021-12-21 09:57:28 +0100

    added raising of error when student's name does not consist of at least two words

--- projekt/main.py
+++ projekt/main.py
@@ -3,3 +3,11 @@ class E_gradebook:
         self.students = []
         self.index = 0
 
+    def string_error(self, string):
+        if type(string) is not str:
+            raise TypeError(f'Given value: "{string}" is not a string')
+
+    def add_student(self, student):
+        self.string_error(student)
+        if len(student.split()) < 2:
+            raise ValueError("Student must have a name and a surname")
