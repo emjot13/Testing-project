98c396dc8c43ba397dd5a1b80da5dd9ab4519cee
Mateusz Jarzembiński <mateuszjarzembinski@wp.pl>
2021-12-21 10:44:05 +0100

    tests of automatic index increment and adding one more instancetest

--- projekt/tests/tests_pyhamcrest.py
+++ projekt/tests/tests_pyhamcrest.py
@@ -18,4 +18,35 @@ class Tests(unittest.TestCase):
     def test_correct_instances_reproducibility(self):
         assert_that(all_of(instance_of(E_gradebook), self.one, self.two, self.three))
 
+    def test_assert_init_index_is_int(self):
+        assert_that(self.temp.index, instance_of(int))
 
+    def test_increased_index_after_adding_student(self):
+        before = self.temp.index
+        self.temp.add_student("Kasia K")
+        after = self.temp.index
+        assert_that(after - before), equal_to(1)
+
+    def test_increased_index_after_adding_10_students(self):
+        for x in range(10):
+            self.temp.add_student("Kasia K")
+        before = self.temp.index
+        self.temp.add_student("Kasia K")
+        after = self.temp.index
+        assert_that(after - before), equal_to(1)
+
+    def test_increased_index_after_adding_100_students(self):
+        for x in range(100):
+            self.temp.add_student("Kasia K")
+        before = self.temp.index
+        self.temp.add_student("Kasia K")
+        after = self.temp.index
+        assert_that(after - before), equal_to(1)
+
+    def test_increased_index_after_adding_1000_students(self):
+        for x in range(1000):
+            self.temp.add_student("Kasia K")
+        before = self.temp.index
+        self.temp.add_student("Kasia K")
+        after = self.temp.index
+        assert_that(after - before), equal_to(1)
