962eb8b4834d227b820d180f3803286df3dc01da
Mateusz Jarzembiński <mateuszjarzembinski@wp.pl>
2021-12-23 18:36:40 +0100

    added tests for incorrect data types for arguments of edit_subject method

--- projekt/tests/tests_DocTests.py
+++ projekt/tests/tests_DocTests.py
@@ -145,6 +145,85 @@ class Tests_delete_subject:
     ValueError: Student with this index does not attend given subject
     """
 
+class Tests_edit_subject:
+    """
+    >>> obj = E_gradebook()
+    >>> obj.add_student("Kasia K")
+    >>> obj.add_subject(0, "matma")
+    >>> obj.edit_subject("0", "matma", "polski")      # tests for incorrect index values
+    Traceback (most recent call last):
+    ...
+    TypeError: Index must be an integer
+    >>> obj.edit_subject([0], "matma", "polski")           # list
+    Traceback (most recent call last):
+    ...
+    TypeError: Index must be an integer
+    >>> obj.edit_subject(0.0, "matma", "polski")           # float
+    Traceback (most recent call last):
+    ...
+    TypeError: Index must be an integer
+    >>> obj.edit_subject(None, "matma", "polski")          # none
+    Traceback (most recent call last):
+    ...
+    TypeError: Index must be an integer
+    >>> obj.edit_subject(True, "matma", "polski")          # bool
+    Traceback (most recent call last):
+    ...
+    TypeError: Index must be an integer
+    >>> obj.edit_subject({0:0}, "matma", "polski")          # dict
+    Traceback (most recent call last):
+    ...
+    TypeError: Index must be an integer
+    >>> obj.edit_subject(0, 0,"polski")                    # tests for incorrect subject name values int
+    Traceback (most recent call last):
+    ...
+    TypeError: Given value: "0" is not a string
+    >>> obj.edit_subject(0, ["matma"], "polski")            # list
+    Traceback (most recent call last):
+    ...
+    TypeError: Given value: "['matma']" is not a string
+    >>> obj.edit_subject(0, 0.0, "polski")                  # float
+    Traceback (most recent call last):
+    ...
+    TypeError: Given value: "0.0" is not a string
+    >>> obj.edit_subject(0, None, "polski")                 # none
+    Traceback (most recent call last):
+    ...
+    TypeError: Given value: "None" is not a string
+    >>> obj.edit_subject(0, False, "polski")                # bool
+    Traceback (most recent call last):
+    ...
+    TypeError: Given value: "False" is not a string
+    >>> obj.edit_subject(0, {"subject":"matma"}, "polski") # dict
+    Traceback (most recent call last):
+    ...
+    TypeError: Given value: "{'subject': 'matma'}" is not a string
+    >>> obj.edit_subject(0, "matma", 0)                    # tests for incorrect updated subject name values int
+    Traceback (most recent call last):
+    ...
+    TypeError: Given value: "0" is not a string
+    >>> obj.edit_subject(0, "matma", ["polski"])            # list
+    Traceback (most recent call last):
+    ...
+    TypeError: Given value: "['polski']" is not a string
+    >>> obj.edit_subject(0, "matma", 0.0)                  # float
+    Traceback (most recent call last):
+    ...
+    TypeError: Given value: "0.0" is not a string
+    >>> obj.edit_subject(0, "matma", None)                 # none
+    Traceback (most recent call last):
+    ...
+    TypeError: Given value: "None" is not a string
+    >>> obj.edit_subject(0, "matma", False)                # bool
+    Traceback (most recent call last):
+    ...
+    TypeError: Given value: "False" is not a string
+    >>> obj.edit_subject(0, "matma", {"subject":"polski"}) # dict
+    Traceback (most recent call last):
+    ...
+    TypeError: Given value: "{'subject': 'polski'}" is not a string
+    """
+
 
 
 if __name__ == "__main__":
