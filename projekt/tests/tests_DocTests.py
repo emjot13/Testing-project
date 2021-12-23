7b9ea5e32f61a1608076f2058a40101728b95448
Mateusz Jarzembiński <mateuszjarzembinski@wp.pl>
2021-12-23 16:33:04 +0100

    added handling of incorrect index and subject names values

---
+++ projekt/tests/tests_DocTests.py
@@ -0,0 +1,61 @@
+from projekt.main import E_gradebook
+
+
+class Tests:
+    """
+    >>> obj = E_gradebook()
+    >>> obj.add_student("Kasia K")              # tests for incorrect index values
+    >>> obj.add_subject("0", "matma")           # string
+    Traceback (most recent call last):
+    ...
+    TypeError: Index must be an integer
+    >>> obj.add_subject([0], "matma")           # list
+    Traceback (most recent call last):
+    ...
+    TypeError: Index must be an integer
+    >>> obj.add_subject(0.0, "matma")           # float
+    Traceback (most recent call last):
+    ...
+    TypeError: Index must be an integer
+    >>> obj.add_subject(None, "matma")          # none
+    Traceback (most recent call last):
+    ...
+    TypeError: Index must be an integer
+    >>> obj.add_subject(True, "matma")          # bool
+    Traceback (most recent call last):
+    ...
+    TypeError: Index must be an integer
+    >>> obj.add_subject({0:0}, "matma")          # dict
+    Traceback (most recent call last):
+    ...
+    TypeError: Index must be an integer
+    >>> obj.add_subject(0, 0)                    # tests for incorrect subject name values int
+    Traceback (most recent call last):
+    ...
+    TypeError: Given value: "0" is not a string
+    >>> obj.add_subject(0, ["matma"])            # list
+    Traceback (most recent call last):
+    ...
+    TypeError: Given value: "['matma']" is not a string
+    >>> obj.add_subject(0, 0.0)                  # float
+    Traceback (most recent call last):
+    ...
+    TypeError: Given value: "0.0" is not a string
+    >>> obj.add_subject(0, None)                 # none
+    Traceback (most recent call last):
+    ...
+    TypeError: Given value: "None" is not a string
+    >>> obj.add_subject(0, False)                # bool
+    Traceback (most recent call last):
+    ...
+    TypeError: Given value: "False" is not a string
+    >>> obj.add_subject(0, {"subject":"matma"}) # dict
+    Traceback (most recent call last):
+    ...
+    TypeError: Given value: "{'subject': 'matma'}" is not a string
+    """
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
